eab0422bd1a27db406c2a6fc1265d08685e29f6e
mark@bpelpeople.com <mark@bpelpeople.com@ec02fb37-fecc-134c-a844-9407d5814815>
2010-10-15 12:14:43 +0000

    second attempt at deleting moved code

--- trunk/camel-jmx/src/main/java/com/massfords/camel/jmx/NotificationFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.massfords.camel.jmx;
-
-public class NotificationFormatException extends Exception {
-    public NotificationFormatException(Exception aCausedBy) {
-        super(aCausedBy);
-    }
-}
